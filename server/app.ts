454d2202223b64144e5244d034eb542237e319cf
Emie <emie.vandeveire@student.arteveldehs.be>
2025-05-08 15:27:21 +0200

    cleanup and file directory right

---
+++ server/app.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+// Zeg tegen Express: “We gaan EJS gebruiken voor views/templates”
+app.set('view engine', 'ejs');
+// Zeg ook waar de .ejs bestanden zich bevinden
+app.set('views', path.join(__dirname, 'views'));
+
+// Maak de map `public` beschikbaar voor CSS, afbeeldingen, etc.
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Hiermee kan je formulierdata (POST requests) uitlezen
+app.use(express.urlencoded({ extended: true }));
+
+// Routes importeren (bijvoorbeeld ingrediëntenlijst)
+import ingredientsRoutes from './routes/ingredients';       //werkt nog niet want je moet nog iets exporteren in ingredients.ts
+app.use('/ingredients', ingredientsRoutes);           // Route beschikbaar maken op /ingredients
+
+// Als iemand naar de hoofdpagina / gaat, doorsturen naar /ingredients
+app.get('/', (req, res) => {
+  res.redirect('/ingredients');
+});
+
+// Start de server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
